c515d4c99061ce8317587ac4fbcbafd66dc746e0
Esteban Lopez <eruano@kubo.co>
2021-10-26 09:07:52 -0500

    Delete Expenses and Income functionality added

--- pages/movements.vue
+++ pages/movements.vue
@@ -2,41 +2,54 @@
   <div>
     <div v-if="!$store.getters.loading">
       <h1 class="title-incomes">Ingresos</h1>
-    <a-list :grid="{ gutter: 16, column: 4 }" :data-source="incomes">
-      <a-list-item slot="renderItem" slot-scope="item, index">
-        <a-card :title="item.date">
-          <template slot="actions" class="ant-card-actions">
-            <a-icon key="setting" type="setting" />
-            <a-icon key="edit" type="edit" />
-            <a-icon key="ellipsis" type="ellipsis" />
-          </template>
-          <a-card-meta :title="item.name" :description="item.description">
-          </a-card-meta>
-        </a-card>
-      </a-list-item>
-    </a-list>
-    <h1 class="title-expenses">Gastos</h1>
-     <a-list :grid="{ gutter: 16, column: 4 }" :data-source="expenses">
-      <a-list-item slot="renderItem" slot-scope="item, index">
-        <a-card :title="item.name">
-
-          <template slot="actions" class="ant-card-actions">
-            <a-icon key="setting" type="setting" />
-            <a-icon key="edit" type="edit" />
-            <a-icon key="ellipsis" type="ellipsis" />
-          </template>
-          <a-card-meta title="Card title" description="This is the description">
-            <a-avatar
-              slot="avatar"
-              src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-            />
-          </a-card-meta>
-        </a-card>
-      </a-list-item>
-    </a-list>
+      <a-list :grid="{ gutter: 16, column: 4 }" :data-source="incomes">
+        <a-list-item slot="renderItem" slot-scope="item, index">
+          <a-card :title="item.date">
+            <template slot="actions" class="ant-card-actions">
+              <a-icon key="setting" type="setting" />
+              <a-icon key="edit" type="edit" />
+              <a-dropdown :trigger="['click']">
+                <a class="ant-dropdown-link">
+                  <a-icon key="ellipsis" type="ellipsis" />
+                </a>
+                <a-menu slot="overlay">
+                  <a-menu-item key="0"  @click="deleteIncome(item.id)">
+                    Eliminar
+                  </a-menu-item>
+                </a-menu>
+              </a-dropdown>
+            </template>
+            <a-card-meta :title="item.name" :description="item.description">
+            </a-card-meta>
+          </a-card>
+        </a-list-item>
+      </a-list>
+      <h1 class="title-expenses">Gastos</h1>
+      <a-list :grid="{ gutter: 16, column: 4 }" :data-source="expenses">
+        <a-list-item slot="renderItem" slot-scope="item, index">
+          <a-card :title="item.date">
+            <template slot="actions" class="ant-card-actions">
+              <a-icon key="setting" type="setting" />
+              <a-icon key="edit" type="edit" />
+              <a-dropdown :trigger="['click']">
+                <a class="ant-dropdown-link">
+                  <a-icon key="ellipsis" type="ellipsis" />
+                </a>
+                <a-menu slot="overlay">
+                  <a-menu-item key="0" @click="deleteExpense(item.id)">
+                    Eliminar
+                  </a-menu-item>
+                </a-menu>
+              </a-dropdown>
+            </template>
+            <a-card-meta :title="item.name" :description="item.description">
+            </a-card-meta>
+          </a-card>
+        </a-list-item>
+      </a-list>
     </div>
     <div v-else>
-        <img src="~/assets/loading.gif" alt="" class="loading">
+      <img src="~/assets/loading.gif" alt="" class="loading" />
     </div>
   </div>
 </template>
@@ -57,7 +70,7 @@ export default {
   },
   methods: {
     async getIncomes() {
-      this.$store.commit('setLoading',true);
+      this.$store.commit("setLoading", true);
       Axios({
         method: "get",
         url: "http://localhost:3000/getAllIncomes/",
@@ -68,7 +81,7 @@ export default {
         .then(res => {
           console.log(res.code);
           console.log(res);
-          this.$store.commit('setLoading',false);
+          this.$store.commit("setLoading", false);
           if (res.data.code == 100) {
             console.log(res.data.data);
             this.incomes = res.data.data;
@@ -77,7 +90,49 @@ export default {
           }
         })
         .catch(err => {
-          this.$store.commit('setLoading',false);
+          this.$store.commit("setLoading", false);
+          console.log("Error", err);
+        });
+    },
+    deleteIncome(id){
+        Axios({
+        method: "post",
+        url: "http://localhost:3000/deleteIncome/"+id,
+        headers: {
+          "OPC-Auth-Token": this.$store.getters["auth/authToken"]
+        }
+      })
+        .then(res => {
+          console.log(res.code);
+          console.log(res);
+          if (res.data.code == 100) {
+            this.getIncomes();
+          } else {
+            console.log("Error", res);
+          }
+        })
+        .catch(err => {
+          console.log("Error", err);
+        });
+    },
+    deleteExpense(id){
+        Axios({
+        method: "post",
+        url: "http://localhost:3000/deleteExpense/"+id,
+        headers: {
+          "OPC-Auth-Token": this.$store.getters["auth/authToken"]
+        }
+      })
+        .then(res => {
+          console.log(res.code);
+          console.log(res);
+          if (res.data.code == 100) {
+            this.getExpenses();
+          } else {
+            console.log("Error", res);
+          }
+        })
+        .catch(err => {
           console.log("Error", err);
         });
     },
@@ -119,17 +174,16 @@ export default {
   font-size: 1.5em;
 }
 
-.loading{
+.loading {
   margin: 0 auto;
   width: 50%;
 }
 
-#image-income{
+#image-income {
   transition: 0.5s ease-in-out;
 }
 
-#image-income:hover{
+#image-income:hover {
   transform: rotate(15deg);
 }
-
 </style>
